31577b0d7875e6d4887b1fe3c56106ea25cc5ede
ColtonBarnacle <colton.barnacle@students.fhu.edu>
2024-02-15 10:09:16 -0600

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,6 +1,6 @@
 #Revmoe pass and complete the cat class
 import cat
-class Cat():
+class cat():
     def __init__(self):
         self.name = 'unknown'
         self.age = 0
@@ -15,11 +15,11 @@ class Cat():
 #Create objects here
 #These should NOT be indented inside the class
 
-stella = Cat()
+stella = cat()
 stella.name = 'Stella'
 stella.age = 7
 
 
-Garfield = Cat()
+Garfield = cat()
 Garfield.name = 'Garfield'
 Garfield.age = 50
